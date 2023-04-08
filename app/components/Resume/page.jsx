a392e6ec8fcd3537ffb78ce73eaa7b7e5bd9108a
N1se7en <nabosunia@gmail.com>
2023-04-08 07:29:00 +0600

    resume QR code rounded shape

--- app/components/Resume/page.jsx
+++ app/components/Resume/page.jsx
@@ -21,7 +21,7 @@ export default function Resume() {
           }}
           whileInView={{ opacity: 1, x: 0 }}
           className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
-            md:rounded-lg md:w-74 md:h-95 xl:w-[500px] xl:h-[500px]"
+            md:rounded-full xl:rounded-full md:w-74 md:h-95 xl:w-[500px] xl:h-[500px]"
           src="/CV.jpg"
         />
         <div className="space-y-10 px-2 md:px-10">
